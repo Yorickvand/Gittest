e26155028e808c4ac0654376f4315bc3172b467b
Yorick <ytm.vandijk@student.avans.nl>
2020-04-15 13:46:57 +0200

    doet ie t

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("Hello World!");
 		
 		System.out.println("Krijg allemaal de kleren");
 		
 		System.out.println("Ding dong");
+
+		System.out.println("Val voor mijn part allemaal dood.");
+
 	}
 
 }
